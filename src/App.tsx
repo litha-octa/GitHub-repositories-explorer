7e55c256c4973e3acb1c5d45dd72c6a01def8b22
litha <lithaoctadelistia@gmail.com>
2023-09-05 21:36:37 +0700

    display

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Center, Input, Text } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
+  // const [data, setData] = React.useState();
+  const [query, setQuery] = React.useState<string>("");
+  let data = [];
+
+  const _handlerQuery = (event: any) => {
+    setQuery(event.target.value);
+  };
+  const _handlerSearch = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="body">
+      <Text fontSize="2xl" className="title" color={"black"}>
+        Search Github User
+      </Text>
+      <Center className="searchbar">
+        <Input
+          variant="flushed"
+          focusBorderColor="blue.400"
+          placeholder="Search Username"
+          value={query}
+          onChange={_handlerQuery}
+          className="query"
+        />
+        <Button
+          leftIcon={<SearchIcon />}
+          onClick={() => _handlerSearch()}
+          colorScheme="linkedin"
+          width={"100%"}
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </Button>
+      </Center>
+      {data.length > 0 ? (
+        <div>
+          <Text
+            className="textResult"
+            fontSize={"md"}
+          >{`Showing users for "${query}"`}</Text>
+        </div>
+      ) : (
+        <Text
+          className="textResult"
+          fontSize={"md"}
+        >{`Users "${query}" not found`}</Text>
+      )}
     </div>
   );
 }
