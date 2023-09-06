e96ed82b8f6b6b0b8d21a4b908e7d22d26a9bc48
litha <lithaoctadelistia@gmail.com>
2023-09-06 07:35:57 +0700

    integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Center, Input, Text } from "@chakra-ui/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import "./App.css";
-import { Button, Center, Input, Text } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
-  // const [data, setData] = React.useState();
-  const [query, setQuery] = React.useState<string>("");
-  let data = [];
+  const [users, setUsers] = useState<any>();
+
+  const [query, setQuery] = useState<string>("");
+  const [urlRepo, setUrlRepo] = useState<string>();
 
   const _handlerQuery = (event: any) => {
     setQuery(event.target.value);
   };
-  const _handlerSearch = () => {};
+
+  const _handlerSearch = async () => {
+    let data = await fetch(`https://api.github.com/search/users?q=${query}`);
+    let json = await data.json();
+    setUsers(json.items.slice(0, 5));
+  };
+
+  const UserItem = ({ item }: any) => {
+    const [isCollaps, setIsCollaps] = useState(true);
+    const [repos, setRepos] = useState<any>();
+    const _handlerGetRepo = async (x: any) => {
+      let data = await fetch(x || "");
+      let json = await data.json();
+      setRepos(json);
+      console.log(json);
+    };
+    return (
+      <Box width={"100%"}>
+        <Box
+          className="userList"
+          onClick={() => {
+            setIsCollaps(!isCollaps);
+            _handlerGetRepo(item.repos_url);
+          }}
+        >
+          <Text fontSize={"25px"}>{item?.login || "username"}</Text>
+          {isCollaps ? (
+            <ChevronDownIcon boxSize={30} />
+          ) : (
+            <ChevronUpIcon boxSize={30} />
+          )}
+        </Box>
+        <Box style={isCollaps ? { display: "none" } : { display: "contents" }}>
+          {repos ? (
+            repos?.map((_item: any, index: number) => {
+              return (
+                <Box key={index} className="repoList">
+                  <Text className="repoTitle">{_item.name}</Text>
+                  <Text>{_item.description || "No description"}</Text>
+                </Box>
+              );
+            })
+          ) : (
+            <Text>no repos</Text>
+          )}
+        </Box>
+      </Box>
+    );
+  };
   return (
-    <div className="body">
-      <Text fontSize="2xl" className="title" color={"black"}>
-        Search Github User
-      </Text>
+    <Box className="body">
       <Center className="searchbar">
         <Input
-          variant="flushed"
-          focusBorderColor="blue.400"
-          placeholder="Search Username"
+          placeholder="Enter username"
           value={query}
           onChange={_handlerQuery}
           className="query"
         />
-        <Button
-          leftIcon={<SearchIcon />}
-          onClick={() => _handlerSearch()}
-          colorScheme="linkedin"
-          width={"100%"}
-        >
+        <Button onClick={() => _handlerSearch()} className="btn-submit">
           Search
         </Button>
+
+        {users ? (
+          users?.map((item: any, index: number) => {
+            return <UserItem item={item} key={index} />;
+          })
+        ) : (
+          <Text>null</Text>
+        )}
       </Center>
-      {data.length > 0 ? (
-        <div>
-          <Text
-            className="textResult"
-            fontSize={"md"}
-          >{`Showing users for "${query}"`}</Text>
-        </div>
-      ) : (
-        <Text
-          className="textResult"
-          fontSize={"md"}
-        >{`Users "${query}" not found`}</Text>
-      )}
-    </div>
+    </Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,8 @@ root.render(
   </ChakraProvider>
 );
 
+//TOKEN :: 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
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
